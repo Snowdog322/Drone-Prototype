a69e11ffdd4a729683cbc161135de64c1454c08c
snowdog322 <ryba@ryba.pl>
2024-11-23 23:47:21 +0100

    made code basics for engine control

--- drone.ino
+++ drone.ino
@@ -1,20 +1,91 @@
-const int buttonPin = 2;
-const int ledPin = 13;
+const int upPin = 1;
+const int downPin = 2;
+
+const int forwardPin = 3;
+const int backwardPin = 4;
+
+const int leftPin = 5;
+const int rightPin = 6;
 
 
-int buttonState = 0;
+const int engine4 = 13;
+const int engine3 = 12;
+const int engine2 = 11;
+const int engine1 = 10;
+
+
+int up_state = 0;
+int down_state = 0;
+int forward_state = 0;
+int backward_state = 0;
+int left_state = 0;
+int right_state = 0;
+
+void up(){
+  digitalWrite(engine1, HIGH);
+  digitalWrite(engine2, HIGH);
+  digitalWrite(engine3, HIGH);
+  digitalWrite(engine4, HIGH);
+}
+
+void down(){
+  digitalWrite(engine1, LOW);
+  digitalWrite(engine2, LOW);
+  digitalWrite(engine3, LOW);
+  digitalWrite(engine4, LOW);
+}
+
+void forward(){
+  digitalWrite(engine1, LOW);
+  digitalWrite(engine2, LOW);
+  digitalWrite(engine3, HIGH);
+  digitalWrite(engine4, HIGH);
+}
+
+void backward(){
+  digitalWrite(engine1, HIGH);
+  digitalWrite(engine2, HIGH);
+  digitalWrite(engine3, LOW);
+  digitalWrite(engine4, LOW);
+}
+
+void left(){
+  digitalWrite(engine1, LOW);
+  digitalWrite(engine2, HIGH);
+  digitalWrite(engine3, LOW);
+  digitalWrite(engine4, HIGH);
+}
+
+void right(){
+  digitalWrite(engine1, HIGH);
+  digitalWrite(engine2, LOW);
+  digitalWrite(engine3, HIGH);
+  digitalWrite(engine4, LOW);
+}
+
+
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  pinMode(engine1, OUTPUT);
+  pinMode(engine2, OUTPUT);
+  pinMode(upPin, INPUT);
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
+  up_state = digitalRead(upPin);
 
-  if (buttonState == HIGH) {
-    digitalWrite(ledPin, HIGH);
-  } else {
-    digitalWrite(ledPin, LOW);
+  forward_state = digitalRead(engine3);
+  backward_state = digitalRead(engine4);
+
+
+
+  if (up_state == HIGH) {
+    up();
+  } 
+  else if(forward_state == HIGH) {
+    forward();
+  }
+  else{
+    down();
   }
 }
